8e48176bbab32cf39f263349f67d050d44bf6363
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2023-08-14 12:06:23 +0700

    cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import express from 'express'
 import morgan from 'morgan'
 import routes from './routes.js'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -23,6 +24,7 @@ const app = express()
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
 app.use(express.json())
 
